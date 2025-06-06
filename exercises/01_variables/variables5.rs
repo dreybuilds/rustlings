0eaab0fb5308154762cabd855fa274c87ef7efef
Audrey <a.nzilani@kokonetworks.com>
2025-06-06 22:43:06 +0300

    Update exercises and runner-crate with latest changes

---
+++ exercises/01_variables/variables5.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut number = "T-H-R-E-E"; // Don't change this line
+    println!("Spell a number: {}", number);
+
+    // TODO: Fix the compiler error by changing the line below without renaming the variable.
+    let int_number = 3;
+    println!("Number plus two is: {}", int_number + 2);
+}
